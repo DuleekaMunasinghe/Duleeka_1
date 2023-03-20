4520352a304d48d6025cc21b4ff6b8d0452e53fb
Duleeka.Munasinghe <duleekalakm@gmail.com>
2023-03-20 21:17:12 +1300

    Program for Access Specifier1

---
+++ src/Test1/accessSpecifiers1.java
@@ -0,0 +1,21 @@
+
+//1. Class is having Default access modifier 
+package Test1;
+class defAccessSpecifier
+{ 
+	void display() 
+	{ 
+		System.out.println("You are using defalut access specifier"); 
+	} 
+} 
+
+public class accessSpecifiers1 {
+
+	public static void main(String[] args) {
+		//default
+		System.out.println("Dafault Access Specifier");
+		defAccessSpecifier obj = new defAccessSpecifier(); 		// how to access non - sub class , obj is the name of themethod and it doesn't matter wheather it changes or not  
+		obj.display(); 
+
+	}
+}
